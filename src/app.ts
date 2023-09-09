7624bdb68013ac9397a695c382e6333888ea999e
AGUSTINA PATT <agustinapatt@gmail.com>
2023-09-09 12:41:37 +0100

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
 import { getHistoricalBalance } from "./services/getHistoricalBalances";
 
+
 const app = express();
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get("/historical-balances", (req, res) => {
+app.get("/historical-balances/query", (req, res) => {
   const historicalBalance = getHistoricalBalance();
   return res.json(historicalBalance);
 });
+
 
 export default app;
