b009b13ff700b6c7662b98781fb5de6c096aa2c1
AGUSTINA PATT <agustinapatt@gmail.com>
2023-09-11 19:18:33 +0100

    started getHistoricalBalance.test

--- test/unit/getHistoricalBalance.test.ts
+++ test/unit/getHistoricalBalance.test.ts
@@ -2,10 +2,37 @@ import { getHistoricalBalances } from "../../src/services/getHistoricalBalances"
 
 describe("getHistoricalBalance", () => {
   it("should return the boilerplate object", () => {
-    let transactionData =
+    const transactionsData = [
+      { 
+          "amount": -765, 
+          "currency": "EUR", 
+          "date": "2022-06-30T09:57:27.235Z", 
+          "status": "BOOKED" 
+      }, 
+      { 
+        "amount": 78, 
+        "currency": "EUR", 
+        "date": "2022-06-28T09:57:27.235Z", 
+        "status": "BOOKED" 
+    }, 
+    { 
+      "amount": -765, 
+      "currency": "EUR", 
+      "date": "2022-05-28T09:57:27.235Z", 
+      "status": "BOOKED" 
+  }, 
+  ];
 
+  const balanceData = { 
+    "amount": 10000, 
+    "currency": "EUR", 
+    "date": "2022-06-30T23:59:59.577Z" 
+};
+ 
+  const sort = "desc";
+  const to = "2022-05-30";
 
-    const res = getHistoricalBalances();
+    const res = getHistoricalBalances(transactionsData, balanceData, to, sort);
     expect(res).toEqual( [ 
       { 
           "date": "05/01/2022", 
