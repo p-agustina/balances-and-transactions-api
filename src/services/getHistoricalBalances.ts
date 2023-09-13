fd9374f955cde05b4fe73bab7e6cfca7a5533a5f
AGUSTINA PATT <agustinapatt@gmail.com>
2023-09-13 15:14:09 +0100

    updated README file

--- src/services/getHistoricalBalances.ts
+++ src/services/getHistoricalBalances.ts
@@ -1,16 +1,37 @@
-export function getHistoricalBalances(transactionsData: Array<string>, balanceData: Array<string>, to: string, sort: string) {
+type Transaction = {
+  amount: number;
+  currency: string;
+  date: string;
+  status: string;
+};
+
+type BalanceData = {
+  amount: number;
+  currency: string;
+  date: string;
+};
+
+export function getHistoricalBalances(transactionsData: Transaction[], balanceData: BalanceData, to: string, sort: string) {
   let historicalBalances = [];
+
+  console.log("transactionsData: ", transactionsData);
+
   let sortedTransactions = transactionsData.sort(
     (a, b) => new Date(b.date) - new Date(a.date)
   );
 
   let currentDate = new Date("2022-06-30T23:59:59.577Z");
+  console.log("Here is the currentDate: ", currentDate);
+  
   let dailyBalance = balanceData.amount;
   for (const transaction of sortedTransactions) {
       
     while (currentDate >= new Date(transaction.date)) {
         
         dailyBalance -= transaction.amount;
+
+        // const formattedDate = 
+
         historicalBalances.push({
             date: currentDate.toISOString().substring(0, 10),
             amount: dailyBalance,
@@ -22,5 +43,6 @@ export function getHistoricalBalances(transactionsData: Array<string>, balanceDa
   let filteredBalances = historicalBalances.filter((balance) => {
     return new Date(balance.date) <= new Date(to);
   });
+  console.log("ACA ESTA EL RESULTADO DE getHistoricalBalances", filteredBalances)
   return filteredBalances;
 }
--- test/unit/getHistoricalBalance.test.ts
+++ test/unit/getHistoricalBalance.test.ts
@@ -2,53 +2,58 @@ import { getHistoricalBalances } from "../../src/services/getHistoricalBalances"
 
 describe("getHistoricalBalance", () => {
   it("should return the boilerplate object", () => {
+    
     const transactionsData = [
-      { 
-          "amount": -765, 
-          "currency": "EUR", 
-          "date": "2022-06-30T09:57:27.235Z", 
-          "status": "BOOKED" 
-      }, 
-      { 
-        "amount": 78, 
-        "currency": "EUR", 
-        "date": "2022-06-28T09:57:27.235Z", 
-        "status": "BOOKED" 
-    }, 
-    { 
-      "amount": -765, 
-      "currency": "EUR", 
-      "date": "2022-05-28T09:57:27.235Z", 
-      "status": "BOOKED" 
-  }, 
-  ];
+      {
+        amount: -32,
+        currency: "EUR",
+        date: "2022-06-30T09:57:27.235Z",
+        status: "BOOKED",
+      },
+      {
+        amount: 137,
+        currency: "EUR",
+        date: "2022-06-29T09:57:27.235Z",
+        status: "BOOKED",
+      },
+      {
+        amount: -765,
+        currency: "EUR",
+        date: "2022-06-28T09:57:27.235Z",
+        status: "BOOKED",
+      },
+    ];
 
-  const balanceData = { 
-    "amount": 10000, 
-    "currency": "EUR", 
-    "date": "2022-06-30T23:59:59.577Z" 
-};
- 
-  const sort = "desc";
-  const to = "2022-05-30";
+    const balanceData = {
+      amount: 10000,
+      currency: "EUR",
+      date: "2022-06-30T23:59:59.577Z",
+    };
+
+    const sort = "desc";
+    const to = "2022-06-30";
+
+    const mockResponse = [
+      {
+        date: "30/06/2022",
+        amount: 10032,
+        currency: "EUR",
+      },
+      {
+        date: "29/06/2022",
+        amount: 9895,
+        currency: "EUR",
+      },
+      {
+        date: "28/06/2022",
+        amount: 10660,
+        currency: "EUR",
+      },
+    ];
 
     const res = getHistoricalBalances(transactionsData, balanceData, to, sort);
-    expect(res).toEqual( [ 
-      { 
-          "date": "05/01/2022", 
-          "amount": 12345, 
-          "currency": "EUR" 
-      }, 
-      { 
-          "date": "04/01/2022", 
-          "amount": 12345, 
-          "currency": "EUR" 
-      }, 
-      { 
-          "date": "03/01/2022", 
-          "amount": 12345, 
-          "currency": "EUR" 
-      }
-  ]  );
+    console.log("ACA ESTA EL RESSSSSSS", res)
+    
+    expect(res).toEqual(mockResponse);
   });
 });
